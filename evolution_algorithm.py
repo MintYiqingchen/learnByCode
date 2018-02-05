d0d255e12785739f99d6eb819c49783d70f9bce3
unknown <821244608@qq.com>
2018-02-05 20:47:47 +0800

    add trade man solution in evolution algorithm

--- evolution_algorithm.py
+++ evolution_algorithm.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from pprint import pprint
 DNA_SIZE=10		# DNA长度
 POP_SIZE=100	# 种群样本总数
 CROSSOVER_RATE=0.8	# 交配率
@@ -38,25 +39,125 @@ def object_F(x):
 	'''目标函数'''
 	return np.sin(10*x)*x+np.cos(2*x)*x
 
-plt.ion()       # something about plotting
-x = np.linspace(*X_BOUND, 200)
-plt.plot(x, object_F(x))
+def demo():
+	plt.ion()       # something about plotting
+	x = np.linspace(*X_BOUND, 200)
+	plt.plot(x, object_F(x))
 
-pop = np.random.randint(2,size=(POP_SIZE,DNA_SIZE))
-for _ in range(N_GENERATIONS):
-	scores = get_fitness(translateDNA(pop))
+	pop = np.random.randint(2,size=(POP_SIZE,DNA_SIZE))
+	for _ in range(N_GENERATIONS):
+		scores = get_fitness(translateDNA(pop))
 
-	if 'sca' in globals(): 
-		sca.remove()
-	sca = plt.scatter(translateDNA(pop), scores, lw=0, c='red', alpha=0.5);
-	plt.pause(0.05)
+		if 'sca' in globals(): 
+			sca.remove()
+		sca = plt.scatter(translateDNA(pop), scores, lw=0, c='red', alpha=0.5);
+		plt.pause(0.05)
 
-    # GA part (evolution)
-	print("Most fitted DNA: ", pop[np.argmax(scores), :], np.max(scores))
-	parent = select(pop, scores)
-	pop = parent.copy()
-	for i,p in enumerate(pop):
-		child = crossover(p, parent)
-		child = mutate(child)
-		pop[i]=child
-plt.ioff(); plt.show()
+	    # GA part (evolution)
+		print("Most fitted DNA: ", pop[np.argmax(scores), :], np.max(scores))
+		parent = select(pop, scores)
+		pop = parent.copy()
+		for i,p in enumerate(pop):
+			child = crossover(p, parent)
+			child = mutate(child)
+			pop[i]=child
+	plt.ioff(); plt.show()
+
+class GA(object):
+	def __init__(self, dnas,popsize, maxgeneration):
+		self.popsize = popsize
+		self.maxiter = maxgeneration
+		self.dna_size = dnas
+		self.output_value = self._objectf
+	def _crossover(self,p, parents):
+		raise NotImplementedError
+	def _objectf(self,x):
+		raise NotImplementedError
+	def _fitness(self,x):
+		res = self._objectf(x)
+		res = res-min(res)+1e-3
+		return res
+	def _select(self,pop, fitness):
+		#print('sum:',fitness.sum())
+		idx = np.random.choice(range(self.popsize), size=self.popsize, p = fitness/fitness.sum())
+		pop = pop[idx]
+		return pop
+
+	def _mutate(self,child):
+		raise NotImplementedError
+	def _translate(self,pop):
+		raise NotImplementedError
+	def _rand_generate(self):
+		'''default method '''
+		return np.random.randint(0,2, size=(self.popsize, self.dna_size))
+	def evolve(self):
+		pop = self._rand_generate()
+		for _ in range(self.maxiter):
+			x = self._translate(pop)
+			scores = self._fitness(x)
+			yield {'result':x[np.argmin(scores)],
+					'score':self.output_value([x[np.argmin(scores)]])[0]}
+			pop = self._select(pop, scores)
+			parent = pop.copy()
+			for i,p in enumerate(pop):
+				child = self._crossover(p, parent)
+				child = self._mutate(child)
+				pop[i]=child
+
+class TradeMan(GA):
+	def __init__(self,dnas,popsize,maxgeneration, adj):
+		super(TradeMan,self).__init__(dnas,popsize,maxgeneration)
+		self.adj = adj
+		self.dna_size = adj.shape[0]
+		self.output_value = self._getdistance
+	def _getdistance(self,x):
+		res=np.zeros(len(x))
+		for i in range(len(x)):
+			cost = 0.0
+			for j in range(len(x[i])):
+				if j==len(x[i])-1:
+					cost += self.adj[x[i][j]][x[i][0]]
+				else:
+					cost += self.adj[x[i][j]][x[i][j+1]]
+			res[i]=cost
+		return res
+	def _objectf(self,x):
+		res = self._getdistance(x)
+		return np.exp(1/res)
+	def _translate(self,pop):
+		return pop
+	def _crossover(self,p, parents):
+		if np.random.rand()<CROSSOVER_RATE:
+			idx = np.random.choice(range(len(parents)))
+			other = parents[idx]
+			b = np.random.randint(0,2,size=self.dna_size,dtype=np.bool)
+			child = np.where(b, p, [-1]*self.dna_size)
+			remain_city = [x for x in other if x not in child]
+			for i in range(len(child)):
+				if child[i]==-1:
+					child[i]=remain_city.pop(0)
+			return child
+		else:
+			return p
+	def _mutate(self,child):
+		if np.random.rand()<0.003:
+			i,j = np.random.choice(range(self.dna_size),size=2)
+			child[i], child[j] = child[j], child[i]
+		return child
+	def _rand_generate(self):
+		pop = np.array([np.random.permutation(self.dna_size) for _ in range(self.popsize)])
+		return pop
+
+def trademan_demo():
+	np.random.seed(7)
+	graph = np.random.rand(10,10)*10
+	pprint(graph)
+	for i in range(10):
+		graph[i][i] = 0.0
+	tm = TradeMan(10,100,200,graph)
+	for a in tm.evolve():
+		print(a)
+
+if __name__ == '__main__':
+	#demo()
+	trademan_demo()
